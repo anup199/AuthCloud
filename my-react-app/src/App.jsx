03e784a6a97a3379a985624e8278bcca8bb1e987
Anup Singh <anupsingh6625@gmail.com>
2026-04-13 17:55:22 +0530

    add route and remove lint to header

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,10 @@ import Home from './Pages/Home'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
 import Login from './Pages/Login'
+import List from './Pages/List'
+import ApplyForm from './Pages/ApplyForm'
+import Layout from './Components/Layout'
+
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
@@ -20,29 +24,26 @@ function App() {
 
   return (
     <>
-   
-    <button onClick={() => {
+    {/* <button style={{ width:"100px" }} onClick={() => {
       localStorage.removeItem('token');
       setIsAuthenticated(false);
-    }}>Logout</button>
+    }}>Logout</button> */}
 
     <div className="container">
+    
     <BrowserRouter>
-      <nav>
-        <Link to="/home">Home</Link> | 
-        <Link to="/about"> About</Link> | 
-        <Link to="/contact"> Contact</Link>
-      </nav>
+    <Layout>
       <Routes>
         <Route path="/home"  element={<Home />} />
         <Route path="/about"  element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/applyNow" element={<ApplyForm />} />
+        <Route path="/list" element={<List />} />
       </Routes>
+      </Layout>
     </BrowserRouter>
+    
     </div>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, amet nobis. Voluptate dolor, placeat blanditiis repudiandae sint omnis deserunt eius reprehenderit aperiam repellat perferendis distinctio voluptatum fugit, eveniet totam sapiente?
-    </p>
     </>
   )
 }
