d0c30903ef912839f45f83319a2f0a794ca2eed6
Anup Singh <anupsingh6625@gmail.com>
2026-04-13 17:57:08 +0530

    footer section

---
+++ my-react-app/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer style={{ background: "#222", color: "#fff", padding: "10px", textAlign: "center" }}>
+      © 2026 AuthCloud. All rights reserved.
+    </footer>
+    )
+}
+
+export default Footer
